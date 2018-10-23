02eeccc8d6a109ca07e774e20c9d7e7cf8e83ea2
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-23 17:05:47 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,17 @@
-class Application 
+require 'pry'
 
-end 
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    binding.pry
+    if req.path.match(/items/)
+      @@items.each do |i|
+        resp.write "#{item}"
+      end
+
+  end
+end
