6bb4b9b4b63ec3de17dfbf2fd49dc9a8f059fc0c
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-23 17:25:43 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,9 +10,9 @@ class Application
 
     if req.path.match(/items/)
       item_searched = req.path.split("/items/").last
-      item_matched  = @@items.find { |i| i.name == item_searched}
-      if item_matched == item_searched
-        resp.write "#{item_searched.price}"
+      # item_matched  = @@items.find { |i| i.name == item_searched}
+      if item_matched = @@items.find { |i| i.name == item_searched}
+        resp.write item_matched.price
       else
         resp.status = 400
         resp.write "Item not found"
